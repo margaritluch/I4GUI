b274b14b7c50fc6a9db70521f0a7bd4a7e8cf825
Margarit Ghubatyan <au534498@uni.au.dk>
2019-01-30 18:46:08 +0100

    initial solution

---
+++ Lesson1_GameHiLo/GameHiLo/GameHiLo/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace GameHiLo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            int guessedNumber;
+            bool isGuessed = false;
+
+            Console.WriteLine("***" + " " + "Welcome to the Hi_Lo game" + " " + "***");
+            Console.WriteLine("The computer choose a number between 1 and 100, you guess it");
+            number = new Random().Next(1, 100);
+
+            while (!isGuessed)
+            {
+                Console.WriteLine("Enter your guess: ");
+                guessedNumber = int.Parse(Console.ReadLine());
+
+                if (guessedNumber > number)
+                {
+                    Console.WriteLine("You guess is too high");
+                }
+                else if (guessedNumber < number)
+                {
+                    Console.WriteLine("Your guess is too low");
+                }
+                else
+                {
+                    isGuessed = true;
+                }
+            }
+            Console.WriteLine("YOU GOT IT!");
+            Console.ReadKey();
+        }
+    }
+}

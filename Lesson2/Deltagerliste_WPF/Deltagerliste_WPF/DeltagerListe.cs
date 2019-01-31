d0093be65e5667d9eaae9a5f3507771efc0ef2cf
Margarit Ghubatyan <au534498@uni.au.dk>
2019-01-31 20:37:31 +0100

    initial commit

---
+++ Lesson2/Deltagerliste_WPF/Deltagerliste_WPF/DeltagerListe.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Deltagerliste_WPF
+{
+    public class DeltagerListe
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Role { get; set; }
+        public string Available { get; set; }
+        public string Origin { get; set; }
+        public char StudieNummer { get; set; }
+        public char Email { get; set; }
+
+        public DeltagerListe(int id, string name, char studieNummer, char email,string role, string available, string origin )
+        {
+            ID = id;
+            Name = name;
+            StudieNummer = studieNummer;
+            Email = email;
+            Role = role;
+            Available = available;
+            Origin = origin;
+        }
+    }
+}
---
+++ Lesson2/Deltagerliste_WPF/Deltagerliste_WPF/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace Deltagerliste_WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            MyList.ItemsSource = ReadCSV();
+        }
+
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+        //public IEnumerable<DeltagerListe> ReadCSV(string fileName)
+        public IEnumerable<DeltagerListe> ReadCSV()
+        {
+            
+            string path = @"C:\Users\Bruger\Desktop\GUI\02 deltagerliste.csv";
+       
+            //string[] lines = File.ReadAllLines(System.IO.Path.ChangeExtension(fileName, ".csv"));
+            string[] lines = File.ReadAllLines(path);
+   
+            return lines.Select(line =>
+            {
+                string[] data = line.Split(';');
+                
+                // We return a person with the data in order.
+                //ToDO this line needs to e formatted, errorprone
+                return new DeltagerListe(Int32.Parse(data[0]),data[1], Convert.ToChar(data[2]), Convert.ToChar(data[3]),data[4], data[5], data[6]);
+            });
+ 
+        }
+    }
+}

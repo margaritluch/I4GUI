54c34b92a601db3117f08a1db6b93c6c9614962c
Magi <margaritdk@gmail.com>
2019-02-13 21:59:02 +0100

    initial commit, solution constructed

---
+++ Lesson5/USBabyNamesAnalyze/Lab05_analyzingUSBabyNames/Lab05_analyzingUSBabyNames/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lab05_analyzingUSBabyNames
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly List<BabyName> babies;
+        private readonly StreamReader sr;
+        private string path = @"babynames.txt";
+        private List<int> decades;
+        private string[,] topNames;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            babies= new List<BabyName>();
+            sr = new StreamReader(path);
+                  
+        }
+
+        private void lstDecadeTopNames_Loaded(object sender, RoutedEventArgs e)
+        {
+            while (!sr.EndOfStream)
+            {
+             
+                babies.Add(new BabyName(sr.ReadLine()));
+            }
+
+            lstDecadeTopNames.ItemsSource = babies;
+        }
+
+        private void decadeName_Loaded(object sender, RoutedEventArgs e)
+        {
+            decades = new List<int>() {1900, 1910, 1920, 1930, 1940, 1950, 1960, 1970, 1980, 1990, 2000};
+            decadeName.ItemsSource = decades;
+             //topNames = new string[babies,];
+        }
+
+   
+    }
+}
---
+++ Lesson5/USBabyNamesAnalyze/Lab05_analyzingUSBabyNames/Part1/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Windows;
+
+
+namespace Part1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string path = @"babynames.txt";
+  
+        public MainWindow()
+        {
+            InitializeComponent();  
+        }
+
+        private void lstDecadeTopNames_Loaded(object sender, RoutedEventArgs e)
+        {
+            /* eventhandler that is reading the text file and showing the first 10 lines in the listbox
+             later on add excepthion handler for in case text file is empty*/
+            var lines = File.ReadAllLines(path).Take(10);
+            lstDecadeTopNames.ItemsSource = lines;
+        }
+    }
+}

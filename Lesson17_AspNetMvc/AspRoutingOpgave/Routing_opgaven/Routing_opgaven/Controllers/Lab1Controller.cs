ea663aac9d829cacf444b20c35ac364f70a0e3ac
Magi <margaritdk@gmail.com>
2019-03-26 09:49:20 +0100

    using routing

---
+++ Lesson17_AspNetMvc/AspRoutingOpgave/Routing_opgaven/Routing_opgaven/Controllers/Lab1Controller.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Routing_opgaven.Controllers
+{
+    public class Lab1Controller : Controller
+    {
+        public IActionResult ShowResult (int x = 2, int y = 4)
+        {
+            return Content(string.Format($"x={x}, og y={y}"));
+        }
+    }
+}
---
+++ Lesson17_AspNetMvc/AspRoutingOpgave/Routing_opgaven/Routing_opgaven/Controllers/Lab3Controller.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Routing_opgaven.Controllers
+{
+    public class Lab3Controller : Controller
+    {
+        public IActionResult AddPerson()
+        {
+          
+        }
+    }
+}
